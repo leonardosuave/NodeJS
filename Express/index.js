e018d6031e26df51538b130285ab7d9edcf63157
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-07-21 18:59:45 -0300

    att

--- Express/index.js
+++ Express/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express(); //express() é a função que vai carregar todo o express na variavel.
 
+app.get('/', (req, res) => {
+    res.send('Bem vindo a página!')
+}) ;
+
 app.listen(4000, (erro) => {
     if(erro) {
         console.log('Ocorreu um erro!');
     }else {
         console.log('Servidor iniciado com sucesso!')
-    }
-})
+        console.log('http://localhost:4000')
+    };
+});
