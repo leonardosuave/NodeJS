c277130872c177d68e2dd9e10663e6c70083b5b0
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-07-21 14:05:48 -0300

    modules

---
+++ Modulos/app.js
@@ -0,0 +1,6 @@
+const calculadora = require('./calculadora')
+
+
+console.log(calculadora.soma(10, 20))
+console.log(calculadora.divisao(10, 20))
+console.log(calculadora.subtracao(10, 20))
---
+++ Modulos/calculadora.js
@@ -0,0 +1,17 @@
+function soma(a, b) {
+    return a + b
+};
+
+function divisao(a, b) {
+    return a/b
+}
+
+function subtracao(a, b) {
+    return a-b
+}
+
+module.exports = {
+    soma,
+    divisao,
+    subtracao
+} 
