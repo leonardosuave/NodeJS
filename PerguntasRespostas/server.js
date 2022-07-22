f95b4771dec37ce84822d292ff04352771f3cc41
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-07-22 17:43:07 -0300

    review views

--- PerguntasRespostas/server.js
+++ PerguntasRespostas/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
+//View engine Express
+app.set('view engine', 'ejs');
 
+//Rotas
 app.get('/', (req, res) => {
-    res.send('Bem vindo ao meu site!')
+    res.render('index')
 });
 
-app.listen(8080), () => {
-    console.log('app rodando!')
-}
+//Escuta
+app.listen(8080, () => {
+    console.log('Server conectado!')
+    console.log()
+    console.log('http://localhost:8080')
+});
