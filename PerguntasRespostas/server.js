3585b91e5fefd3a325f1b698ecf6b71ed5b77cf8
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-07-24 15:40:50 -0300

    update project

--- PerguntasRespostas/server.js
+++ PerguntasRespostas/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');      //View engine Express
 app.use(express.static('public'));  //Arquivos estáticos
+
+app.use(express.urlencoded({extended:false})) //Permite coletar os dados do forms dentro da aplicação (decodifica os dados do forms)
+app.use(express.json()) //Permite a leitura de dados de forms enviado via json.
 
 //Rotas
 app.get('/', (req, res) => {
@@ -11,6 +15,12 @@ app.get('/', (req, res) => {
 app.get('/perguntar', (req, res) => {
     res.render('perguntar')
 });
+
+app.post('/salvarPergunta', (req, res) => {
+    const titulo = req.body.titulo
+    const descricao = req.body.descricao
+    res.send(`Formulário respondido! Titulo:${titulo}; Descricao:${descricao}`)  
+})
 
 //Escuta do server
 app.listen(8080, () => {
