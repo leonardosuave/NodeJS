acad62ce10392a8bff008902af1eca38ce82936a
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-07-25 02:26:55 -0300

    update connection MySQL in project

--- PerguntasRespostas/server.js
+++ PerguntasRespostas/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
+const connection = require('./database/database');
+
+//Conection database
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão feita com o banco de dados!')
+        app.emit('pronto') //sinal emitido de conexão.
+    })
+    .catch((msgErro) => {
+        console.log(msgErro)
+    });
 
 app.set('view engine', 'ejs');      //View engine Express
 app.use(express.static('public'));  //Arquivos estáticos
@@ -22,9 +33,12 @@ app.post('/salvarPergunta', (req, res) => {
     res.send(`Formulário respondido! Titulo:${titulo}; Descricao:${descricao}`)  
 })
 
-//Escuta do server
-app.listen(8080, () => {
-    console.log('Server conectado!')
-    console.log()
-    console.log('http://localhost:8080')
+//Para iniciar o BD antes do servidor.
+app.on('pronto', () =>{
+    //Escuta do server
+    app.listen(8080, () => {
+        console.log('Server conectado!')
+        console.log()
+        console.log('http://localhost:8080')
+    });
 });
