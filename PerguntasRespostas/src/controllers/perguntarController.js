27ee1f9d3d3c1996c690db7a7519d7aba456b4dd
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-07-25 20:24:37 -0300

    Project Ask and Answer anonymous complete.

--- PerguntasRespostas/src/controllers/perguntarController.js
+++ PerguntasRespostas/src/controllers/perguntarController.js
@@ -1,6 +1,7 @@
 const Perguntar = require('../models/perguntarModel'); //Tratamento de dados por model da controladora 
 
 const perguntaModel = require('../../database/Pergunta') //Para acessar o banco de dados caso seja tratado os dados apenas na controladora diretamente (pergunta.Controller.responder)
+const RespostaModel = require('../../database/Resposta');
 
 exports.index = (req, res) => {
     res.render('perguntar')
@@ -18,8 +19,14 @@ exports.responderPergunta = async (req, res) => {
         where: {id: req.params.id}
     });
 
-    if(perguntaBD != undefined) {
-        res.render('responder', {perguntaBD})
+    if(perguntaBD !== undefined) {
+        const respostaBD = await RespostaModel.findAll({
+            where: {perguntaID: perguntaBD.id}, raw: true, order:[
+                ['createdAt', 'DESC'] //Se for encontrado o perguntaBD então sera verificado se existe alguma resposta referente a esta pergunta.
+        ]})
+        console.log(respostaBD)
+
+        res.render('responder', {perguntaBD, respostaBD})
     } else {
         res.render('404')
     };
