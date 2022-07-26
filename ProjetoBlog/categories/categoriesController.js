5b1cd2b7afe77cecc676d2334abf344680435459
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-07-26 19:05:32 -0300

    att

---
+++ ProjetoBlog/categories/categoriesController.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const route = express.Router();
+
+/*
+route.get('/categories', (req, res) => {
+    res.send('Organizado por categoria')
+});
+
+module.exports = route
+
+
+OBS: Em server importar essa rota -> importado na const categoriesController;
+
+Após isso, executar pelo express as rotas importadas pelo categoriesController -> app.use('/'. categoriesController) -> '/' é prefixo e também opcional
+
+*/
+
