505799c3ec6fe2d9cff137f6f2f76de3105d3eb6
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-08-10 12:08:48 -0300

    First API REST complete with DB fake

--- IntroduçãoAPI_REST/APIGAMES/server.js
+++ IntroduçãoAPI_REST/APIGAMES/server.js
@@ -5,7 +5,7 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json())
 
 //Banco de dados falso em JSON(pode substituir por mysql posteriormente)
-const BD = {
+let BD = {
 
     games: [
         {
@@ -68,6 +68,104 @@ app.get('/game/:id', (req, res) => {
         
     }
     
+});
+
+app.post('/game', (req, res) => {
+    const {title, year, produtora, price} = req.body
+
+    if(!title || !produtora) {
+        res.sendStatus(400)
+
+    } else if (isNaN(year) || isNaN(price)) {
+        res.sendStatus(400)
+    } else {
+        const year = parseInt(req.body.year)
+        const price = parseInt(req.body.price)
+
+        BD.games.push({
+            id: 11,
+            title,
+            year, 
+            produtora,
+            price
+        })
+
+        res.sendStatus(200)
+    }
+});
+
+app.delete('/game/:id', (req, res) => {
+    if(isNaN(req.params.id)) {
+        res.sendStatus(400)
+    } else {
+        const id = parseInt(req.params.id)
+
+        const index = BD.games.findIndex(g => g.id == id)
+
+        if(index == -1) {
+            res.sendStatus(404)
+        } else {
+            BD.games.splice(index, 1)
+            res.sendStatus(200)
+        }
+    }
+});
+
+app.put('/game/:id', (req, res) => {
+
+    if(isNaN(req.params.id)) {
+        res.sendStatus(400)
+    } else {
+        const id  = parseInt(req.params.id)
+        const game = BD.games.find(g => g.id == id)
+
+        if(game != undefined) {
+
+            const {title, year, produtora, price} = req.body
+
+            if(title != undefined) {
+
+                if(!title) {
+                    res.sendStatus(400)
+                }
+
+                game.title = title
+            }
+
+            if(year != undefined) {
+
+                if (isNaN(year)) {
+                    res.sendStatus(400)
+
+                }
+                  
+                game.year = year
+            }
+
+            if(produtora != undefined) {
+
+                if(!produtora) {
+                    res.sendStatus(400)
+                }    
+
+                game.produtora = produtora
+            }
+
+            if(price != undefined) {
+
+                if (isNaN(price)) {
+                    res.sendStatus(400)
+                }    
+                
+                game.price = price
+            }
+
+            res.sendStatus(200)
+
+        } else {
+            res.sendStatus(404)
+        }
+    }
 })
 
 app.listen(3030, ()=> {
