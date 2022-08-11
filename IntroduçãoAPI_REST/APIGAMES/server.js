1af5cac5709c87eb84f0375a883bb3e6f4b2c5df
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-08-11 19:11:31 -0300

    Create Authenticate JTW

--- IntroduçãoAPI_REST/APIGAMES/server.js
+++ IntroduçãoAPI_REST/APIGAMES/server.js
@@ -1,10 +1,44 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+
+//Senha para criptografar
+const JWTsecret = 'soiqlkqmweapapajkwmjsfdyhshajjwunngccrwaqiyyutuweeew'
 
 app.use(cors())
 app.use(express.urlencoded({extended:false}));
 app.use(express.json())
+
+//Middleware
+function auth(req, res, next) {
+    const authToken = req.headers['authorization']   
+    //console.log(authToken) -> Verá bearer + o token de validação
+    if(authToken != undefined) {
+
+        const bearer = authToken.split(' ') //Para separar o bearer do token de validação
+        const token = bearer[1] //Para utilizar apenas o token de validação
+
+        jwt.verify(token, JWTsecret, (erro, data) => {
+            if(erro) {
+                res.status(401);
+                res.json({erro: 'Token inválido'})
+            } else {
+                //console.log(data) -> Retorna o conteúdo do token, sendo id, email, quando foi gerado e data de expiração do token.
+
+                //Criar essas duas variáveis dentro do objeto de req dentro do middleware -> É importante para poder acessar essa duas variáveis dentro de qualquer endPoint que utilize este middleware
+                req.token = token;
+                req.loggedUser = {id: data.id, email: data.email} // informação do usuario logado
+                next() 
+            }
+        })
+
+    } else {
+        res.status(401)
+        res.json({erro:'Token inválido'})
+    }
+    
+}
 
 //Banco de dados falso em JSON(pode substituir por mysql posteriormente)
 let BD = {
@@ -65,32 +99,39 @@ app.post('/auth', (req, res) => {
         const user = BD.users.find( u => u.email == email) //Se encontrar um usuário significa que o email enviado na req está cadastrado
 
         if(user != undefined) {
-
             if(user.password == password) {
-                res.status(200)
-                res.json({token: 'TOKEN FALSO'})
+                //Cria a assinatura jwt. 
+                //O primeiro termo passa informações essenciais do usuário ->PayLoad
+                //É uma função assincrona, por isso o callback(err,token)
+                jwt.sign({id: user.id, email: user.email}, JWTsecret, {expiresIn: '48h'}, (erro, token) => {
+                    if(erro) {
+                        res.status(400);
+                        res.json({erro: 'Falha interna'})
+                    } else {
+                        res.status(200);
+                        res.json({token: token})
+                    }
+                })
             } else {
                 res.status(401);
                 res.json({erro: 'Credenciais inválidas!'})
             }
-
         } else {
             res.status(404);
             res.json('O email não está registrado.')
         }
-
     } else {
         res.status(400);
         res.json('Email inválido')
     }
 })
 
-app.get('/games',(req, res) => {
-    res.statusCode(200); //Status de success
+app.get('/games', auth, (req, res) => {
+    res.statusCode =200; //Status de success
     res.json(BD.games);
 });
 
-app.get('/game/:id', (req, res) => {
+app.get('/game/:id', auth, (req, res) => {
 
     //Necessário verificar se id é um número
     if(isNaN(req.params.id)) {
@@ -114,7 +155,7 @@ app.get('/game/:id', (req, res) => {
     
 });
 
-app.post('/game', (req, res) => {
+app.post('/game', auth, (req, res) => {
     const {title, year, produtora, price} = req.body
 
     if(!title || !produtora) {
@@ -138,7 +179,7 @@ app.post('/game', (req, res) => {
     }
 });
 
-app.delete('/game/:id', (req, res) => {
+app.delete('/game/:id', auth, (req, res) => {
     if(isNaN(req.params.id)) {
         res.sendStatus(400)
     } else {
@@ -155,7 +196,7 @@ app.delete('/game/:id', (req, res) => {
     }
 });
 
-app.put('/game/:id', (req, res) => {
+app.put('/game/:id', auth, (req, res) => {
 
     if(isNaN(req.params.id)) {
         res.sendStatus(400)
