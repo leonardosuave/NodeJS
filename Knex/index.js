6296e101aac6b0ae41e033b96dd27e899733bed0
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-08-17 02:01:31 -0300

    att

--- Knex/index.js
+++ Knex/index.js
@@ -1,1 +1,10 @@
 const database = require('./database')
+
+const dados = {
+    nome: 'Resident Evil 3 - NEMESIS',
+    price: 37.5
+}
+
+//Retorna uma promisse
+const dado = database.insert(dados).into('game')
+console.log(dado)
