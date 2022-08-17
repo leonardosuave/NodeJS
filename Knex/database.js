58927768ff31a291249e165d2e560185ccdd3b33
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-08-17 03:37:18 -0300

    Learning to use Queries Builders with HeidiSQL

--- Knex/database.js
+++ Knex/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
       host : 'localhost',
       user : 'root',
       password : process.env.CONNECTIONSTRING,
-      database : 'knextjs'
+      database : 'knexjs'
     }
   });
 
--- Knex/index.js
+++ Knex/index.js
@@ -1,10 +1,76 @@
 const database = require('./database')
-
-const dados = {
-    nome: 'Resident Evil 3 - NEMESIS',
-    price: 37.5
-}
+/*
+const dados = [
+    {
+    name: 'Resident Evil 2 - Remake',
+    price: 85
+    },
+    {
+    name: 'Resident Evil 4 - Remake',
+    price: 120
+    },
+    {
+    name: 'Resident Evil 8',
+    price: 75
+    }
+]
 
 //Retorna uma promisse
-const dado = database.insert(dados).into('game')
-console.log(dado)
+database.insert(dados).into('game').then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+/* SELECT
+database.select(['id', 'price']).table('game').then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+/*NESTED QUERIES -> Uma query de busca dentro de outra query de cadastro
+database.insert({name: 'Silent Hill', price: 27.5}).into('game').then(data => {
+    database.select('*').table('game').then(data => {
+        console.log(data)
+    }).catch(err => {
+        console.log(err)
+    })
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+/*WHERE
+database.select('*').whereRaw('price < 50').table('game').then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+/*DELETE
+database.where({id: 3}).delete().table('game').then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+/*UPDATE
+database.where({name: 'Silent Hill'}).update({price: 12.5}).table('game').then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
+*/
+
+//ORDER by
+
+database.select('*').table('game').orderBy('price', 'desc').then(data => {
+    console.log(data)
+}).catch(err => {
+    console.log(err)
+})
