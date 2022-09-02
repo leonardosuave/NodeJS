cd658debdc15690fd8e85a69c037a81f8dec6254
Leonardo Suave <leonardo.suave15@hotmail.com>
2022-09-01 21:56:28 -0300

    emit socket event and disconnect cliente

--- Socket.io/server.js
+++ Socket.io/server.js
@@ -13,8 +13,20 @@ app.use(routes)
 //Evento de conexão sempre que o cliente conecta a aplicação websocket.
 //socket é a instância do cliente conectado na aplicação.
 io.on('connection', (socket) => {
-    console.log(socket);
-    console.log(socket.id); //Todo cliente conectado tem id unico
+
+    //socket -> refere-se ao cliente (cada cliente é um socket diferente)
+
+    //Detecta cliente desconectado
+    socket.on('disconnect', () => {
+        console.log('X desconectou: ' + socket.id)
+    })
+
+    //Emite eventos
+    socket.on('palavra', (data) => {
+        console.log('Executando evento de boas vindas')
+        console.log(data)
+        socket.emit('resultado', data + ' - Guia de estudos Socket.io' )
+    })
 })
 
 http.listen(3131, () => {
